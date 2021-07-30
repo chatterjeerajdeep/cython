b43d670454a0ea9efe3aafb4e9368a65d91ddc51
Rajdeep Chatterjee <chatterjee.rajdeep@tcs.com>
2021-07-30 23:02:05 +0530

    updated testing

--- testing.py
+++ testing.py
@@ -1,6 +1,7 @@
 import timeit
 
-cy = timeit.timeit('''cython_version.test(5000)''', setup='import cython_version', number = 100)
-py = timeit.timeit('''pure_python.test(5000)''', setup = 'import pure_python', number = 100)
+for i in range(1, 1000, 100):
+	cy = timeit.timeit('''cython_version.test(5000)''', setup='import cython_version', number = 1000)
+	py = timeit.timeit('''pure_python.test(5000)''', setup = 'import pure_python', number = 1000)
 
-print("Cython worked faster than python by {} times".format(py/cy))
+	print("Iteration: {},Cython worked faster than python by {} times".format(i, py/cy))
